a37d425d9afe4c2afeb0afe41cea0ae8fcb0f13d
abdirahmanfarah <abdirahman.z.farah@gmail.com>
2019-10-11 11:08:37 -0700

    form

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
+import Form from './Form';
 
 function App() {
 
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
+        <Form />
       </div>
     );
   }
---
+++ smurfs/src/components/Form.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Form() {
+  return (
+    <div>
+      <p> New Smurf</p>
+      <p>Name: <input /></p>
+      <p>Age: <input /></p>
+      <p>Height: <input /></p>
+      <button>Submit</button>
+    </div>
+  )
+}
+
+export default Form;
